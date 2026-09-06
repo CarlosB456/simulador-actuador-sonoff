f95d5bdfb79c002dd9ff44f38ad4af7f0d194c3f
Carlos B <carlosb456@users.noreply.github.com>
2026-09-06 16:34:44 -0300

    Simulador 3D: Actuador 12V con Sonoff Dual y Reles LY2NJ 10A

---
+++ server.js
@@ -0,0 +1,32 @@
+﻿const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 8085;
+const DIR = __dirname;
+
+const mime = {
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.css': 'text/css',
+  '.png': 'image/png'
+};
+
+http.createServer((req, res) => {
+  let file = req.url === '/' ? '/index.html' : req.url.split('?')[0];
+  let filePath = path.join(DIR, file);
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+      return;
+    }
+    res.writeHead(200, {
+      'Content-Type': mime[path.extname(filePath)] || 'application/octet-stream',
+      'Access-Control-Allow-Origin': '*'
+    });
+    res.end(data);
+  });
+}).listen(PORT, () => {
+  console.log('HTTP Server listening on http://localhost:' + PORT);
+});
